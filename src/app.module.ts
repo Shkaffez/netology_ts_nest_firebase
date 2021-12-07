b35e4ac0918e0059d0222685b171fdcdc9aab6d0
Shkaffez <shishkov-i@mail.com>
2021-12-07 21:45:43 +0300

    homework2_2_2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksService } from './books-service/books.service';
+import { BooksController } from './books-controller/books.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, BooksController],
+  providers: [AppService, BooksService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/books-controller/books.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Post, Delete, Body, Param } from '@nestjs/common';
+import { BooksService } from '../books-service/books.service'
+import { Book } from '../book.interface';
+
+@Controller('books')
+export class BooksController {
+  constructor(private bookService: BooksService) { }
+
+  @Get()
+  findAll(): Book[] {
+    return this.bookService.findAll();
+  }
+
+  @Post()
+  create(@Body() book: Book): void {
+    this.bookService.create(book);
+  }
+
+  @Get(':id')
+  findOne(@Param() id: string): Book {
+    return this.bookService.findOne(id);
+  }
+
+  @Delete(':id')
+  delete(@Param() id: string): void {
+    this.bookService.delete(id);
+  }
+
+}

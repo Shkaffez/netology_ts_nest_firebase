bd2e2f8b4d21aa6cd0313fdbaf69ad9a4ce13ccb
Shkaffez <shishkov-i@mail.com>
2021-12-09 22:21:14 +0300

    mogoose schema for book

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksService } from './books-service/books.service';
 import { BooksController } from './books-controller/books.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://shishkov-i:12qwaszx@cluster0.x7d8w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
   controllers: [AppController, BooksController],
   providers: [AppService, BooksService],
 })

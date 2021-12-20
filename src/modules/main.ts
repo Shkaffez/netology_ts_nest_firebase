0728815db8d52f111259b5c87be4d427ff1d353e
Shkaffez <shishkov-i@mail.com>
2021-12-20 21:45:24 +0300

    before tests

--- src/modules/main.ts
+++ src/modules/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { StatusInterseptor } from '../common/interseptors/status.interseptor';
+import { HttpExceptionFilter } from '../common/exception-filters/http.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new StatusInterseptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
